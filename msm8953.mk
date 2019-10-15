f4503b4bc9be4d15b0f71fe475d1711c8320e725
Dhina17 <dhinalogu@gmail.com>
2019-10-15 02:44:35 +0000

    sdm632-common: Make Use Of Stock Hals
    - camera.msm8953
    - libmm-qcamera
    - gralloc.msm8953
    - hwcomposer.msm8953
    - memtrack.msm8953
    - libc2dcolorconvert
    - libmm-omxcore
    - libOmxAacEnc
    - libOmxAmrEnc
    - libOmxCore
    - libOmxEvrcEnc
    - libOmxQcelp13Enc
    - libOmxVdec
    - libOmxVenc
    - libstagefrighthw
    - libsensorndkbridge
    
    Signed-off-by: Dhina17 <dhinalogu@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -111,8 +111,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    camera.msm8953 \
-    libmm-qcamera \
     Snap
 
 # Configstore
@@ -128,10 +126,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    copybit.msm8953 \
-    gralloc.msm8953 \
-    hwcomposer.msm8953 \
-    memtrack.msm8953 \
     libdisplayconfig \
     liboverlay \
     libqdMetaData.system \
@@ -236,20 +230,6 @@ PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
     libandroid_net
 
-# OMX
-PRODUCT_PACKAGES += \
-    libc2dcolorconvert \
-    libmm-omxcore \
-    libOmxAacEnc \
-    libOmxAmrEnc \
-    libOmxCore \
-    libOmxEvrcEnc \
-    libOmxQcelp13Enc \
-    libOmxSwVencHevc \
-    libOmxVdec \
-    libOmxVenc \
-    libstagefrighthw
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.1-service-qti
@@ -305,8 +285,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
-    libsensorndkbridge
+    android.hardware.sensors@1.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
