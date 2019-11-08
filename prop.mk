0991b9ff1257040df9f43b4fa1ade4d070412753
mylove90 <engm.saad1990@gmail.com>
2019-11-08 21:31:25 +0000

    Revert "msm8953-common: Enable QCOM Boost framework"
    This reverts commit 7272d5e66475058e338aa66935710caac2ec87d2.

--- prop.mk
+++ prop.mk
@@ -132,15 +132,6 @@ ro.vendor.qti.sys.fw.bservice_enable=true
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.vendor.extension_library=libqti-perfd-client.so
 
-#QTI Performance
-PRODUCT_PROPERTY_OVERRIDES += \
-vendor.enable_prefetch=1 \
-vendor.iop.enable_uxe=1 \
-vendor.iop.enable_prefetch_ofr=1 \
-vendor.perf.iop_v3.enable=1 \
-ro.vendor.at_library=libqti-at.so \
-persist.vendor.qti.games.gt.prof=1
-
 # Netmgrd
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.vendor.use_data_netmgrd=true \
